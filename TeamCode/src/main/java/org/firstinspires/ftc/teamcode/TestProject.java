94e1b437a2540315b15277bec165b4ede1d614ab
Onkar Dhillon <onkardhillon73@gmail.com>
2023-06-24 00:25:25 -0400

    Add skeleton of 'TestProject.java' to remote.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestProject.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class TestProject extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
