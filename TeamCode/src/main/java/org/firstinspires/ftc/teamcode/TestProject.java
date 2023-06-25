09ec6b514935dabde2a2333d18ee3b9720d25d39
Onkar Dhillon <onkardhillon73@gmail.com>
2023-06-24 21:51:49 -0400

    Added robot init and started skeleton of robot.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestProject.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestProject.java
@@ -1,11 +1,23 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
+@TeleOp(name = "TeleOpTest", group = "TeleOp")
 public class TestProject extends LinearOpMode {
+
+    public Robot robot = new Robot();
 
     @Override
     public void runOpMode() throws InterruptedException {
+
+        robot.init(hardwareMap);
+
+        while(opModeIsActive()){
+            
+        }
+
 
     }
 }
