a5b59f93280a6388b9f46d026f51ed1b96c5cd08
Onkar Dhillon <onkardhillon73@gmail.com>
2023-07-09 19:36:36 -0400

    Final Skeleton of TeleOpModes.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTankDrive.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "BasicTankDrive", group = "TeleOp")
+public class BasicTankDrive extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        // Put hardwareMap below.
+
+        // Put initialization blocks here.
+
+        waitForStart();
+
+        if(opModeIsActive()){
+            // Put run blocks here.
+            while(opModeIsActive()){
+                // Put loop blocks here.
+            }
+        }
+
+    }
+}
